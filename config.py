e7017cd0ae9b154d8a2897659fce9ab12260bea9
Karam <skaram20c@gmail.com>
2025-12-01 15:32:24 -0500

    Initial commit with Configuration update.

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 MYSQL_HOST = "localhost"
 MYSQL_USER = "root"
-MYSQL_PASSWORD = "W@heguru2013"
+MYSQL_PASSWORD = "###########@123"
 MYSQL_DATABASE = "FraudDetectionDB"
-SECRET_KEY = "secret"
+SECRET_KEY = "secret_key"
